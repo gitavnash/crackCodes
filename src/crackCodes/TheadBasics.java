58128740adf8c2334525e4f056cd3ff0438b9335
gitavnash <kumaravnash45@gmail.com>
2021-06-03 17:23:49 +0530

    Adding a java concurrency Example.
    Print Even and Odd number

--- src/crackCodes/TheadBasics.java
+++ src/crackCodes/TheadBasics.java
@@ -1,10 +1,74 @@
 package crackCodes;
 
+import java.util.concurrent.Semaphore;
+
 public class TheadBasics {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-       System.out.println("Calling to end the section");
+	public static void main(String[] args) throws InterruptedException {
+
+		sharedprinter sp = new sharedprinter();
+
+		Thread tt = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				int i = 1;
+				while (i < 25) {
+					try {
+						sp.printEven(i);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} finally {
+						i = i + 2;
+					}
+
+				}
+			}
+
+		});
+
+		Thread tt1 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				int i = 0;
+				while (i < 25) {
+					try {
+						sp.printOdd(i + 2);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					i = i + 2;
+				}
+			}
+
+		});
+
+		tt.start();
+		tt1.start();
+
+		tt.join();
+
+		tt1.join();
 	}
 
+}
+
+class sharedprinter {
+	static Semaphore odd = new Semaphore(0);;
+	static Semaphore even = new Semaphore(1);;
+
+	static void printOdd(int n) throws InterruptedException {
+		odd.acquire();
+		System.out.println(n + " ");
+		even.release();
+	}
+
+	static void printEven(int n) throws InterruptedException {
+		even.acquire();
+		System.out.println(n + " ");
+		odd.release();
+	}
 }
