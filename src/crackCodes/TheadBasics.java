87610bd647e12ebcd33bc84af2b388711469692f
gitavnash <kumaravnash45@gmail.com>
2021-06-03 12:42:16 +0530

    First commit

---
+++ src/crackCodes/TheadBasics.java
@@ -0,0 +1,10 @@
+package crackCodes;
+
+public class TheadBasics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Calling to end the section");
+	}
+
+}
